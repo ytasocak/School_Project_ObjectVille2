1be02344e6ef2c361dd9291398de3664c0a1664c
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2026-05-09 01:23:20 +0300

    implement maploader logic (not finished)

--- com/objectville/infrastructure/MapLoader.java
+++ com/objectville/infrastructure/MapLoader.java
@@ -1,9 +1,27 @@
 package com.objectville.infrastructure;
 import com.objectville.model.Cell;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class MapLoader {
     public Cell[][] loadMap(String fileName){
-        // Will add buffer reader and other elements
+        try(BufferedReader br = new BufferedReader(new FileReader(fileName))){
+            String line = br.readLine();
+            line = line.trim();
+            int n = Integer.parseInt(line);
+
+            Cell[][] grid = new Cell[n][n];
+            for (int row = 0; row < n; row++) {
+                String rowLine = br.readLine();
+                for (int column = 0; column < n; column++) {
+                    char symbol = rowLine.charAt(column);
+                }
+            }
+        }catch (IOException e){
+            System.out.println("IO Exception occurred");
+        }
 
         return null;// placeholder
     }
