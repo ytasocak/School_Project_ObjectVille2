74a15bb3a2af84849c150c0a53bbf7d1bc227b2f
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2026-05-08 03:03:36 +0300

    implement base IO infrastructure and update factory method naming

---
+++ com/objectville/infrastructure/MapLoader.java
@@ -0,0 +1,10 @@
+package com.objectville.infrastructure;
+import com.objectville.model.Cell;
+
+public class MapLoader {
+    public Cell[][] loadMap(String fileName){
+        // Will add buffer reader and other elements
+
+        return null;// placeholder
+    }
+}
--- com/objectville/model/CellFactory.java
+++ com/objectville/model/CellFactory.java
@@ -3,7 +3,7 @@ package com.objectville.model;
 import com.objectville.Exceptions.MapValidationException;
 
 public class CellFactory {
-    public Cell CellFactory(char type,int x, int y){
+    public Cell createCell(char type,int x, int y){
         switch (Character.toUpperCase(type)) { // I saw Character.toUpperCase in online to convert char to upper
             case 'E': return new EmptyCell(x, y);
             case 'H': return new Housing(x, y,'H');
