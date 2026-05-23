f7f2ec775d315dbb5e035c03976368e715110caf
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2026-05-23 15:07:52 +0300

    Refactor: Change x and y to row and col because other members uses row and col. Feat: Create and change some part of MapLoader Class.

--- com/objectville/infrastructure/CellFactory.java
+++ com/objectville/infrastructure/CellFactory.java
@@ -5,14 +5,15 @@ import com.objectville.model.base.Cell;
 import com.objectville.model.base.EmptyCell;
 import com.objectville.model.base.Road;
 import com.objectville.model.zone.Housing;
+import com.objectville.model.zone.Industrial;
 
 public class CellFactory {
-    public Cell createCell(char type, int x, int y){
+    public Cell createCell(char type, int row, int col){
         switch (Character.toUpperCase(type)) { // I saw Character.toUpperCase in online to convert char to upper
-            case 'E': return new EmptyCell(x, y);
-            case 'H': return new Housing(x, y,'H');
-            case 'R': return new Road(x,y);
-            case 'I': return null; // NULL AS A PLACEHOLDER
+            case 'E': return new EmptyCell(row, col);
+            case 'H': return new Housing(row, col,'H');
+            case 'R': return new Road(row,col);
+            case 'I': return new Industrial(row,col,'I'); // NULL AS A PLACEHOLDER
             case 'C': return null;
             case 'P': return null;
             case 'W': return null;
@@ -21,7 +22,7 @@ public class CellFactory {
             case 'D': return null;
             case 'S': return null;
             default:
-                throw new MapValidationException("Unknown symbol at (" + x + "," + y + "): " + type);
+                throw new MapValidationException("Unknown symbol at (" + row + "," + col + "): " + type);
     }
 }
 }
--- com/objectville/infrastructure/MapLoader.java
+++ com/objectville/infrastructure/MapLoader.java
@@ -1,28 +1,49 @@
 package com.objectville.infrastructure;
+
 import com.objectville.model.base.Cell;
+import com.objectville.infrastructure.CellFactory;
+import com.objectville.exceptions.MapValidationException;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class MapLoader {
-    public Cell[][] loadMap(String fileName){
-        try(BufferedReader br = new BufferedReader(new FileReader(fileName))){
-            String line = br.readLine();
-            line = line.trim();
-            int n = Integer.parseInt(line);
 
+    public Cell[][] loadMap(String fileName) {
+
+        CellFactory cf = new CellFactory();
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line = br.readLine();
+            if (line == null) {
+                throw new MapValidationException("Map path is correct but file is empty?");
+            }
+
+            int n = Integer.parseInt(line.trim());
             Cell[][] grid = new Cell[n][n];
+
             for (int row = 0; row < n; row++) {
                 String rowLine = br.readLine();
-                for (int column = 0; column < n; column++) {
-                    char symbol = rowLine.charAt(column);
+
+                if (rowLine == null || rowLine.trim().length() != n) {
+                    throw new MapValidationException(
+                            "Map dimensions do not match!!!! Expected length: " + n + ", Error at row: " + row
+                    );
+                }
+
+                String trimmedRow = rowLine.trim();
+                for (int col = 0; col < n; col++) {
+                    char symbol = trimmedRow.charAt(col);
+                    grid[row][col] = cf.createCell(symbol,row,col);
                 }
             }
-        }catch (IOException e){
-            System.out.println("IO Exception occurred");
-        }
 
-        return null;// placeholder
+            return grid;
+
+        } catch (IOException e) {
+            throw new MapValidationException("File reading failed: " + fileName + " Why: " + e.getMessage());
+        } catch (NumberFormatException e) {
+            throw new MapValidationException("Map size (N) is not a valid integer.");
+        }
     }
 }
