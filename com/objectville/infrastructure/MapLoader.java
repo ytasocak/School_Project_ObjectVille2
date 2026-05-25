351f2fcebced7a9fb5ff3da1762107f6cf26b31d
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2026-05-26 00:35:41 +0300

    Refactor: I refactor the code because I thought grid must be N N but it is N M so I changed the IO classes to adapt it.

--- com/objectville/infrastructure/MapLoader.java
+++ com/objectville/infrastructure/MapLoader.java
@@ -7,43 +7,55 @@ import com.objectville.exceptions.MapValidationException;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapLoader {
 
     public Cell[][] loadMap(String fileName) {
-
         CellFactory cf = new CellFactory();
+        List<String> lines = getStrings(fileName);
+
+        int n = lines.size();
+        int m = lines.getFirst().length();
+
+        Cell[][] grid = new Cell[n][m];
+
+        for (int row = 0; row < n; row++) {
+            String rowLine = lines.get(row);
+
+            if (rowLine.length() != m) {
+                throw new MapValidationException(
+                        "Map dimensions do not match! Expected row length: " + m + ", Error at row: " + row
+                );
+            }
+
+            for (int col = 0; col < m; col++) {
+                char symbol = rowLine.charAt(col);
+                grid[row][col] = cf.createCell(symbol, row, col);
+            }
+        }
+
+        return grid;
+    }
+
+    private static List<String> getStrings(String fileName) {
+        List<String> lines = new ArrayList<>();
+
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
-            String line = br.readLine();
-            if (line == null) {
-                throw new MapValidationException("Map path is correct but file is empty?");
-            }
-
-            int n = Integer.parseInt(line.trim());
-            Cell[][] grid = new Cell[n][n];
-
-            for (int row = 0; row < n; row++) {
-                String rowLine = br.readLine();
-
-                if (rowLine == null || rowLine.trim().length() != n) {
-                    throw new MapValidationException(
-                            "Map dimensions do not match!!!! Expected length: " + n + ", Error at row: " + row
-                    );
-                }
-
-                String trimmedRow = rowLine.trim();
-                for (int col = 0; col < n; col++) {
-                    char symbol = trimmedRow.charAt(col);
-                    grid[row][col] = cf.createCell(symbol,row,col);
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (!line.trim().isEmpty()) {
+                    lines.add(line.trim());
                 }
             }
-
-            return grid;
-
         } catch (IOException e) {
             throw new MapValidationException("File reading failed: " + fileName + " Why: " + e.getMessage());
-        } catch (NumberFormatException e) {
-            throw new MapValidationException("Map size (N) is not a valid integer.");
         }
+
+        if (lines.isEmpty()) {
+            throw new MapValidationException("Map path is correct but file is empty?");
+        }
+        return lines;
     }
 }
--- com/objectville/infrastructure/ResultWriter.java
+++ com/objectville/infrastructure/ResultWriter.java
@@ -8,22 +8,25 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class ResultWriter {
-    public void writeResult(String filePath, Cell[][] grid){
-        if(grid == null || grid.length == 0){
+
+    public void writeResult(String filePath, Cell[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
             throw new MapValidationException("Map path is correct but grid is empty");
         }
-        try(BufferedWriter bw = new BufferedWriter(new FileWriter(filePath))) {
-            int n = grid.length;
 
-            for (int row = 0; row<n; row++){
-                for (int col = 0; col<n; col++){
-                   bw.write(grid[row][col].getSymbol());
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filePath))) {
+            int n = grid.length;
+            int m = grid[0].length;
+
+            for (int row = 0; row < n; row++) {
+                for (int col = 0; col < m; col++) {
+                    char symbol = (grid[row][col] != null) ? grid[row][col].getSymbol() : '?';
+                    bw.write(symbol);
                 }
                 bw.newLine();
             }
 
-            System.out.println("Result printed to "+filePath);
-
+            System.out.println("Result printed to " + filePath);
 
         } catch (IOException e) {
             System.err.println("Error while writing the result: " + e.getMessage());
