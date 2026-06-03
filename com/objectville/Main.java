0dfa234f2cd308544cf3b8b1d5c47773b7ac846e
kadiralver55 <kadir.alver@std.ieu.edu.tr>
2026-06-03 05:05:20 +0300

    Refactor Main class
    Fix the important problem of reading, Make more readable and short.

--- com/objectville/Main.java
+++ com/objectville/Main.java
@@ -6,100 +6,100 @@ import com.objectville.model.zone.Zone;
 
 public class Main {
     public static void main(String[] args){
-        String mapFilePath="map.txt";
-        int totalTicks= 5;
-        System.out.println("Starting ObjectVille simulation...");
-        System.out.println("Map File Path: "+ mapFilePath);
-        System.out.println("Total Ticks to Run: "+ totalTicks);
-        System.out.println("----------------------------------------------");
-        MapLoader mapLoader=new MapLoader();
-        Cell[][] grid=null;
-        try {
-            grid= mapLoader.loadMap(mapFilePath);
-            System.out.println("Map successfully loaded!");
-            System.out.println("Map Dimensions: "+grid.length+"x"+grid[0].length);
-        } catch (Exception e){
-            System.err.println("Failed to load map: "+e.getMessage());
+        if(args.length<2){
+            System.err.println("Error: Please enter the map file and the number of ticks.");
+            System.err.println("Usage: java Main <map_file.txt> <tick_count>");
             return;
         }
-        System.out.println("-----------------------------------------------");
-        ServiceAreaManager serviceManager= new ServiceAreaManager();
+        String mapFilePath="map.txt";
+        int totalTicks=5;
+        if(args.length>0){
+            mapFilePath=args[0];
+        }
+        if(args.length>1){
+            try{
+                totalTicks = Integer.parseInt(args[1]);
+            } catch(NumberFormatException e){
 
-        ConnectivityValidator connectivityValidator=new ConnectivityValidator(grid.length,grid[0].length);
-        VisitedTracker visitedTracker= new VisitedTracker(grid.length,grid[0].length);
+            }
+        }
+        System.out.println("Starting ObjectVille simulation...");
+        System.out.println("Map File Path: "+mapFilePath);
+        System.out.println("Total Ticks to Run: "+totalTicks);
+        System.out.println("-------------------------------------------");
+
+        MapLoader mapLoader = new MapLoader();
+        Cell[][] grid = null;
+        try{
+            grid = mapLoader.loadMap(mapFilePath);
+            System.out.println("Map successfully loaded!");
+            System.out.println("Map Dimensions: "+ grid.length+ "x"+grid[0].length);
+        } catch(Exception e){
+            System.err.println("Failed to load map: " + e.getMessage());
+            return;
+        }
+        System.out.println("--------------------------------------------");
+
+        ResultWriter.clearFile("output.txt");
+        ResultWriter writer = new ResultWriter();
+        ServiceAreaManager serviceManager = new ServiceAreaManager();
+        serviceManager.setResultWriter(writer);
+
+        ConnectivityValidator connectivityValidator=new ConnectivityValidator(grid.length, grid[0].length);
+        VisitedTracker visitedTracker= new VisitedTracker(grid.length, grid[0].length);
         UtilityFlowManager utilityManager= new UtilityFlowManager(connectivityValidator,visitedTracker);
+        utilityManager.setResultWriter(writer);
 
         int totalPopulationPool=0;
         int totalGoodsPool=0;
         int totalLifeStylePool=0;
 
-        for(int currentTick=1; currentTick<=totalTicks;currentTick++){
-            System.out.println("Tick :"+currentTick);
-
-            for(int row=0; row<grid.length;row++){
-                for(int col=0;col<grid[0].length;col++){
-                    Cell currentCell=grid[row][col];
+        for(int currentTick=1;currentTick<=totalTicks;currentTick++) {
+            writer.log("Tick " + currentTick);
+            for (int row = 0; row < grid.length; row++) {
+                for (int col = 0; col < grid[0].length; col++) {
+                    Cell currentCell = grid[row][col];
                     if (currentCell instanceof Zone) {
                         Zone z = (Zone) currentCell;
                         z.resetTickData();
                     }
                 }
             }
-        serviceManager.applyServices(grid);
-
-        utilityManager.processGrid(grid);
-
-        int housingCount=0;
-        int commercialCount=0;
-        int industrialCount=0;
-        for(int r=0; r<grid.length;r++){
-            for(int c=0; c<grid[0].length;c++){
-                if(grid[r][c] != null && grid[r][c].getSymbol()=='H'){
-                    housingCount++;
-                }else if(grid[r][c] != null && grid[r][c].getSymbol()=='C'){
-                    commercialCount++;
-                }else if(grid[r][c] != null && grid[r][c].getSymbol()=='I'){
-                    industrialCount++;
-                }
-            }
-        }
-            int populationPerHouse;
-            if(housingCount>0){
-                populationPerHouse=totalPopulationPool/housingCount;
-            }else{
-                populationPerHouse=0;
-            }
-
-            int goodsPerCommercial;
-            if(commercialCount>0){
-                goodsPerCommercial=totalGoodsPool/commercialCount;
-            }else{
-                goodsPerCommercial=0;
-            }
-
-            int lifeStylePerIndustrial;
-            if(industrialCount>0){
-                lifeStylePerIndustrial=totalLifeStylePool/industrialCount;
-            }else{
-                lifeStylePerIndustrial=0;
-            }
-            for(int r=0;r<grid.length;r++){
-                for(int c=0;c<grid[0].length;c++) {
-                    Cell currentCell=grid[r][c];
-
-                    if (currentCell instanceof Zone){
-                        Zone currentZone =(Zone) currentCell;
-                        if(currentCell.getSymbol()=='H'){
-                            currentZone.receivePopulation(populationPerHouse);
-                        }else if(currentCell.getSymbol()=='C'){
-                            currentZone.receiveGoods(goodsPerCommercial);
-                        }else if(currentCell.getSymbol()=='I'){
-                            currentZone.receiveLifestyle(lifeStylePerIndustrial);
-                        }
-                        currentZone.updateZone();
+            serviceManager.applyServices(grid);
+            utilityManager.processGrid(grid);
+            int housingCount = 0;
+            int commercialCount = 0;
+            int industrialCount = 0;
+            for (int r = 0; r < grid.length; r++) {
+                for (int c = 0; c < grid[0].length; c++) {
+                    if (grid[r][c] != null) {
+                        char symbol = grid[r][c].getSymbol();
+                        if (symbol == 'H') housingCount++;
+                        else if (symbol == 'C') commercialCount++;
+                        else if (symbol == 'I') industrialCount++;
                     }
                 }
             }
-        }
+            int lifestylePerHouse = housingCount > 0 ? totalLifeStylePool / housingCount : 0;
+            int goodsPerCommercial = commercialCount > 0 ? totalGoodsPool / commercialCount : 0;
+            int populationPerWorker = (industrialCount + commercialCount) > 0 ? totalPopulationPool / (industrialCount + commercialCount) : 0;
+
+            for (int r = 0; r < grid.length; r++) {
+                for (int c = 0; c < grid[0].length; c++) {
+                    Cell currentCell = grid[r][c];
+                    if (currentCell instanceof Zone) {
+                        Zone currentZone = (Zone) currentCell;
+                        if (currentCell.getSymbol() == 'H') {
+                            currentZone.receiveLifestyle(lifestylePerHouse);
+                        } else if (currentCell.getSymbol() == 'I') {
+                            currentZone.receivePopulation(populationPerWorker);
+                        } else if (currentCell.getSymbol() == 'C') {
+                            currentZone.receivePopulation(populationPerWorker);
+                            currentZone.receiveGoods(goodsPerCommercial);
+                        }
+                    }
+                }
+            }
+        }    
     }
 }
